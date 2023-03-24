49e33b20ac27daa7eacb1c88c4641fa944a3f3b7
Anastasia Dubovik <a.dubovik2010@yandex.ru>
2023-03-24 16:33:27 +0700

    adde condition

---
+++ Example_005_conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "настя")
+{
+     Console.WriteLine("Здарова, босс!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
